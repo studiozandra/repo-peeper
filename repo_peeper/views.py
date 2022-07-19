d6e678afe9b8bc9d865de8a93b0d8a927d125154
Zandra Kubota <zan@studiozandra.com>
2022-07-18 22:13:08 -0400

    add API request, template variables

--- repo_peeper/views.py
+++ repo_peeper/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+import requests
 
 # view table of all repos
 def allrepos(request):
+    response = requests.get("https://api.github.com/users/studiozandra/repos")
+    # for repo in response.json():
+    #     print(repo['name'])
+    #     print(repo['size'])
+    #     print(repo['stargazers_count'])
+    #     print(repo['language'])
+    #     print(repo['html_url'])
 
     context = {
-        'example_context_variable': 'Hey, change me.',
+        'response': response.json(),
     }
 
     return render(request, 'pages/allrepos.html', context)
