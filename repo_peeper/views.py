3d8d0a2d3405e4973c67eec36cd256373639088f
Zandra Kubota <zan@studiozandra.com>
2022-07-19 13:29:32 -0400

    hyperlink URL as btn

--- repo_peeper/views.py
+++ repo_peeper/views.py
@@ -4,15 +4,16 @@ import requests
 # view table of all repos
 def allrepos(request):
     response = requests.get("https://api.github.com/users/studiozandra/repos")
-    # for repo in response.json():
-    #     print(repo['name'])
-    #     print(repo['size'])
-    #     print(repo['stargazers_count'])
-    #     print(repo['language'])
-    #     print(repo['html_url'])
+    response = response.json()
+    for repo in response:
+        repo['name']
+        repo['size']
+        repo['stargazers_count']
+        repo['language']
+        repo['html_url']
 
     context = {
-        'response': response.json(),
+        'response': response,
     }
 
     return render(request, 'pages/allrepos.html', context)
