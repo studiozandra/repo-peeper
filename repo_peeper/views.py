3ea4d23935cf05784d91e79104d41f1df0032d6c
Zandra Kubota <zan@studiozandra.com>
2022-07-21 16:47:06 -0400

    render to data uri

--- repo_peeper/views.py
+++ repo_peeper/views.py
@@ -23,17 +23,17 @@ def allrepos(request):
 
 # repo sizes bar chart
 def repos_by_size(request):
-    bar_chart = pygal.HorizontalBar()
+    bar_chart = pygal.HorizontalBar(show_y_labels=False)
     bar_chart.title = "All repos by size"
     bar_chart.x_labels = map(str, range(1))
     for repo in response:
         bar_chart.add(repo['name'], [repo['size']])
     
-    bar_chart.render('bar_chart.svg')
+    # bar_chart.render('bar_chart.svg')
+    chart_svg_as_datauri = bar_chart.render_data_uri()
 
     context = {
-        'response': response,
-        'bar_chart': bar_chart.render('bar_chart.svg'),
+        "rendered_chart_svg_as_datauri": chart_svg_as_datauri,
     }
 
     return render(request, 'pages/size-chart.html', context)
