545d27c9e6943fe871d33323be59671ff47c1144
Zandra Kubota <zan@studiozandra.com>
2022-07-21 16:17:47 -0400

    populate bar chart

--- repo_peeper/views.py
+++ repo_peeper/views.py
@@ -3,17 +3,17 @@ import requests
 import pygal
 
 
-response = requests.get("https://api.github.com/users/studiozandra/repos")
+response = requests.get("https://api.github.com/users/studiozandra/repos").json()
 
 # view table of all repos
 def allrepos(request):
-    response = response.json()
-    for repo in response:
-        repo['name']
-        repo['size']
-        repo['stargazers_count']
-        repo['language']
-        repo['html_url']
+    
+    # for repo in response:
+    #     repo['name']
+    #     repo['size']
+    #     repo['stargazers_count']
+    #     repo['language']
+    #     repo['html_url']
 
     context = {
         'response': response,
@@ -23,14 +23,16 @@ def allrepos(request):
 
 # repo sizes bar chart
 def repos_by_size(request):
-    bar_chart = pygal.HorizontalStackedBar()
+    bar_chart = pygal.HorizontalBar()
     bar_chart.title = "All repos by size"
-    bar_chart.x_labels = map(str, range(11))
-    bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
-    bar_chart.add('Padovan', [1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12])
+    bar_chart.x_labels = map(str, range(1))
+    for repo in response:
+        bar_chart.add(repo['name'], [repo['size']])
+    
     bar_chart.render('bar_chart.svg')
 
     context = {
+        'response': response,
         'bar_chart': bar_chart.render('bar_chart.svg'),
     }
 
